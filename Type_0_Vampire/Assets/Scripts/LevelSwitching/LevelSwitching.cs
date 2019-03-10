70c8fdfdb74761dd045fa358a9d83482ec2d1ba8
Benjamin Long <39582644+BenLong98@users.noreply.github.com>
2019-03-10 18:09:02 -0400

    new

--- Type_0_Vampire/Assets/Scripts/LevelSwitching/LevelSwitching.cs
+++ Type_0_Vampire/Assets/Scripts/LevelSwitching/LevelSwitching.cs
@@ -6,7 +6,8 @@ public class LevelSwitching : MonoBehaviour {
 
     [SerializeField] GameObject player;
 
-    [SerializeField] GameObject[] levels = new GameObject[3];
+    [SerializeField] GameObject[] levels = new GameObject[4];
+    [SerializeField] GameObject mainSideCam;
 
     [SerializeField] Vector3 playerSpawnPos;
 
@@ -35,7 +36,7 @@ public class LevelSwitching : MonoBehaviour {
                 break;
             case 2:
                 ClearLevels();
-                levels[1].SetActive(true);
+                levels[2].SetActive(true);
                 player.transform.position = playerSpawnPos;
                 break;
             case 3:
@@ -43,6 +44,14 @@ public class LevelSwitching : MonoBehaviour {
                 levels[2].SetActive(true);
                 player.transform.position = playerSpawnPos;
                 break;
+            case 4:
+                ClearLevels();
+                levels[3].SetActive(true);
+                player.gameObject.SetActive(false);
+                mainSideCam.gameObject.SetActive(false);
+
+                break;
+
             default:
                 break;
         }
